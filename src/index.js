4965f48a1f1ea4b011e2e88fe25eefa63561f4e7
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 11:01:28 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<div className='container-fluid vh-100 d-flex align-content-center justify-content-center bg-light'>
+    <div className="card mt-5 text-center shadow-lg" style={{width: " 18rem", height: "33rem"}}>
+        <img src="https://t4.ftcdn.net/jpg/03/00/94/69/360_F_300946931_kSR84OqudEhsmBZH47HU6ud7aZIDMjEx.jpg"
+             className="card-img-top" style={{width: "auto", height: "16rem"}} alt="..."/>
+        <img
+            className="avatar"
+            src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            alt="avatar"
+        />
+        <div className="card-body py-0">
+            <h5 className="card-title">Ruma Khan</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
+                card's content.</p>
+            <a href="#" className="btn btn-primary">Follow</a>
+        </div>
+    </div>
+</div>);
